3dccaec833021d195e6d4f86926bb79ffc93f27d
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-12 17:09:16 -0800

    added another route for gh pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={ <Home /> } />
+          <Route path='/jacobmcanepa' element={ <Home /> } />
           <Route path='/about' element={ <About /> } />
           <Route path='/work' element={ <Work /> } />
           <Route path='/contact' element={ <Contact /> } />
