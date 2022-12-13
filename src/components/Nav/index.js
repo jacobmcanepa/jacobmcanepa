2061718c5a123caa99392bee6c5d8fd740c7e64e
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-12 23:27:09 -0800

    started custom responsive nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 function Nav() {
+  const [navToggler, setNavToggler] = useState(false);
+
+  const clickHandler = () => {
+    setNavToggler(!navToggler);
+  };
+
   return (
-    <ul>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/work">Work</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
+    <nav className='h-auto md:h-24 p2 bg-blue-200 max-w-6xl mx-auto flex justify-between md:items-center'>
+      <Link to='/jacobmcanepa'>
+        <h1>Jacob Canepa</h1>
+      </Link>
+      <div>
+        <NavLink className='nav-link' to='/about'>About</NavLink>
+        <NavLink className='nav-link' to='/work'>Work</NavLink>
+        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
+      </div>
+      <button className='inline md:hidden self-start nav-link' onClick={clickHandler}>
+        {!navToggler ? <FaBars /> : <AiOutlineClose />}
+      </button>
+    </nav>
   );
 }
 
