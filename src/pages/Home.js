1c23e5b2a81e868b8c40e49b2be799738a8e0903
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-14 21:14:57 -0800

    finished mobile home style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,22 +6,22 @@ function Home() {
   return (
     <div className='home'>
       <img src={headshot} alt='jacob-canepa-headshot' className='rounded-full headshot' />
-      <div className='p-2'>
-        <h2 className='font-bold text-3xl'>Full Stack Developer | Video Editor</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus.
+      <div className='p-2 my-4'>
+        <h2 className='font-bold text-5xl'>Full Stack Developer | Video Editor</h2>
+        <p className='text-xl mt-4'>
+          My goal, as a content creator, is to provide quality, cutting-edge products to my clients. 
+          With my passion for creativity, learning, and core technical skills in full-stack web development, 
+          I am confident I will be able help your team achieve its goals.
         </p>
       </div>
-      <div>
-        <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer'>
+      <div className='text-8xl'>
+        <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer' className='m-4'>
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
-        <a href='https://github.com/jacobmcanepa/' target='_blank' rel='noreferrer'>
+        <a href='https://github.com/jacobmcanepa/' target='_blank' rel='noreferrer' className='m-4'>
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' className='m-4'>
           <FontAwesomeIcon icon={faInstagram} />
         </a>
       </div>
