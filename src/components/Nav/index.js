a4aa6ecff630f67b1da3ba4b0b2f6fc67c41274f
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-14 16:23:22 -0800

    finished basic nav functionality; testing on gh pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,14 +11,16 @@ function Nav() {
   };
 
   return (
-    <nav className='h-auto md:h-24 p2 bg-blue-200 max-w-6xl mx-auto flex justify-between md:items-center'>
+    <nav className='h-auto md:h-24 p2 bg-blue-200 mx-auto flex justify-between md:items-center'>
       <Link to='/jacobmcanepa'>
         <h1>Jacob Canepa</h1>
       </Link>
-      <div>
-        <NavLink className='nav-link' to='/about'>About</NavLink>
-        <NavLink className='nav-link' to='/work'>Work</NavLink>
-        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
+      <div className={
+        navToggler ? 'flex flex-col gap-4 md:inline' : 'hidden md:inline'
+      }>
+        <NavLink className='nav-link' to='/about' onClick={clickHandler}>About</NavLink>
+        <NavLink className='nav-link' to='/work' onClick={clickHandler}>Work</NavLink>
+        <NavLink className='nav-link' to='/contact' onClick={clickHandler}>Contact</NavLink>
       </div>
       <button className='inline md:hidden self-start nav-link' onClick={clickHandler}>
         {!navToggler ? <FaBars /> : <AiOutlineClose />}
