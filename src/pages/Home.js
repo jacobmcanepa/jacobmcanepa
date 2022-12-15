e25e856ed7b2fa20747a2476150f8b6afc838b36
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-14 20:42:45 -0800

    homepage start, testing on gh pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,31 @@
+import headshot from '../assets/images/jacob-headshot.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='home'>
+      <img src={headshot} alt='jacob-canepa-headshot' className='rounded-full headshot' />
+      <div className='p-2'>
+        <h2 className='font-bold text-3xl'>Full Stack Developer | Video Editor</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus.
+        </p>
+      </div>
+      <div>
+        <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href='https://github.com/jacobmcanepa/' target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
+    </div>
   );
 }
 
