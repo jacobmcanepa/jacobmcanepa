f926cb56c1c33192b5d07cdd1a7baceb28dacc53
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-18 18:14:21 -0800

    finished mobile about page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={ <Home /> } />
           <Route exact path='/jacobmcanepa' element={ <Home /> } />
-          <Route exact path='/about' element={ <PageNotFound /> } />
+          <Route exact path='/about' element={ <About /> } />
           <Route exact path='/work' element={ <PageNotFound /> } />
           <Route exact path='/contact' element={ <Contact /> } />
           <Route exact path='*' element={ <PageNotFound /> } />
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,23 @@
 function About() {
   return (
-    <h1>This is the about page</h1>
+    <section className="about-section flex flex-col items-center justify-center">
+      <div className="about-card text-center mt-20">
+        <h2 className="font-bold text-7xl mb-4">About</h2>
+        <p className="text-4xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus.
+        </p>
+      </div>
+      <div className="about-card">
+        <h2 className="text-center font-bold text-7xl mb-2">Skills</h2>
+        <ul className="skills-list text-3xl px-4">
+          <li>JavaScript, HTML, CSS, SQL, NoSQL</li>
+          <li>MERN Stack, Handlebars, jQuery, Bootstrap, Tailwind, Jest, Sequelize, Mongoose, GraphQL</li>
+          <li>Premiere Pro, After Effects, Final Cut Pro X, DaVinci Resolve</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
