288ae11437fe3cd198d76ab9e158200f769b6df1
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-19 20:56:24 -0800

    updated flexbox techniques on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,19 @@
 function About() {
   return (
-    <section className="about-section flex flex-col items-center justify-center">
-      <div className="about-card text-center mt-20">
-        <h2 className="font-bold text-7xl mb-4">About</h2>
-        <p className="text-4xl">
+    <section className="about-section flex flex-col justify-center lg:flex-row">
+      <div className="about-card col flex flex-col justify-center items-center">
+        <h2 className="text-8xl font-bold">About</h2>
+        <p className="text-2xl mt-8">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
           condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
           mi, ac feugiat metus aliquam maximus.
         </p>
       </div>
-      <div className="about-card">
-        <h2 className="text-center font-bold text-7xl mb-2">Skills</h2>
-        <ul className="skills-list text-3xl px-4">
+      <div className="about-card col flex flex-col justify-center items-center">
+        <h2 className="text-8xl font-bold">Skills</h2>
+        <ul className="skills-list text-2xl mt-6">
           <li>JavaScript, HTML, CSS, SQL, NoSQL</li>
           <li>MERN Stack, Handlebars, jQuery, Bootstrap, Tailwind, Jest, Sequelize, Mongoose, GraphQL</li>
           <li>Premiere Pro, After Effects, Final Cut Pro X, DaVinci Resolve</li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,11 +10,11 @@ function Home() {
       </div>
       <main className='text-center lg:text-left mx-6'>
         <h2 className='font-bold text-8xl p-2'>Jacob Canepa</h2>
-        <h3 className='font-bold text-6xl p-4'>Full Stack Developer | Video Editor</h3>
+        <h3 className='font-bold text-6xl p-4'>Full Stack Developer | Videographer</h3>
         <p className='text-4xl p-4'>
           My goal, as a content creator, is to provide quality, cutting-edge products to my clients. 
           With my passion for creativity, learning, and core technical skills in full-stack web development as well as 
-          video editing, I am confident I will be able help your team achieve its goals.
+          videography, I am confident I will be able help your team achieve its goals.
         </p>
         <div className="text-8xl text-center">
           <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer' className='secondary-hover'>
