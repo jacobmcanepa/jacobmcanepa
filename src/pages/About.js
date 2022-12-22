2377224e506747101c80917cfc3b9c6b13e023b5
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-21 16:08:58 -0800

    finished about page minus the actual description for the about card

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 function About() {
   return (
     <section className="about-section flex flex-col justify-center lg:flex-row">
-      <div className="about-card col flex flex-col justify-center items-center">
+      <div className="white-card about-card col flex flex-col justify-center items-center">
         <h2 className="text-8xl font-bold">About</h2>
-        <p className="text-2xl mt-8">
+        <p className="text-3xl mt-8 text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
           condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
           mi, ac feugiat metus aliquam maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
@@ -11,9 +11,9 @@ function About() {
           mi, ac feugiat metus aliquam maximus.
         </p>
       </div>
-      <div className="about-card col flex flex-col justify-center items-center">
+      <div className="white-card skills-card col flex flex-col justify-center items-center">
         <h2 className="text-8xl font-bold">Skills</h2>
-        <ul className="skills-list text-2xl mt-6">
+        <ul className="skills-list text-4xl">
           <li>JavaScript, HTML, CSS, SQL, NoSQL</li>
           <li>MERN Stack, Handlebars, jQuery, Bootstrap, Tailwind, Jest, Sequelize, Mongoose, GraphQL</li>
           <li>Premiere Pro, After Effects, Final Cut Pro X, DaVinci Resolve</li>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,9 +6,9 @@ function Contact() {
   return (
     <section className='contact-section flex justify-center items-center text-center'>
       <div className='bg-primary w-full p-6 mt-9 contact-card lg:w-2/3 lg:rounded-xl lg:p-10'>
-        <h2 className='text-7xl font-bold mb-8 lg:text-8xl lg:mb-12'>Like what you see?</h2>
+        <h2 className='text-7xl font-bold mb-8 lg:text-9xl lg:mb-12'>Like what you see?</h2>
         <a href='mailto:jacobmcanepa@gmail.com' 
-        className='rounded-xl p-3 text-4xl font-bold italic email-btn lg:text-5xl'>
+        className='rounded-xl py-3 px-5 text-4xl font-bold italic email-btn lg:text-5xl'>
           Email Me <FontAwesomeIcon icon={faEnvelope} />
         </a>
         <div className='text-7xl mt-8 lg:text-8xl lg:mt-12'>
