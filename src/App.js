0afbc9fa20f950d079774d11d945975257757130
Jacob Canepa <jacobmcanepa@gmail.com>
2022-12-23 04:07:34 -0800

    added WIP pages for deployed site

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
           <Route exact path='/' element={ <Home /> } />
           <Route exact path='/jacobmcanepa' element={ <Home /> } />
           <Route exact path='/about' element={ <About /> } />
-          <Route exact path='/devwork' element={ <DevWork /> } />
-          <Route exact path='/videowork' element={ <VideoWork /> } />
+          <Route exact path='/devwork' element={ <PageNotFound /> } />
+          <Route exact path='/videowork' element={ <PageNotFound /> } />
           <Route exact path='/contact' element={ <Contact /> } />
           <Route exact path='*' element={ <PageNotFound /> } />
         </Routes>
